a6defcda888968e2af9425e6a50b8b1668e07cd3
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-11-23 15:52:52 +0530

    feat: :sparkles: add solution for magic square

--- class-004/magic-square.py
+++ class-004/magic-square.py
@@ -1,11 +1,37 @@
-# def getOddMagicSquare(size: int) -> list[list[int]]:
-
-a = 13
-
-match a:
-    case 1:
-        print("Hi")
-    case 2:
-        print("No...")
-    case _:
-        print("F!!!")
+def getOddMagicSquare(size: int) -> list[list[int]]:
+    magic_square = [[0 for x in range(size)] for y in range(size)]
+    
+    i = size // 2
+    j = size - 1
+    
+    num = 1
+    
+    while num <= (size * size):
+        if i == -1 and j == size:
+            j = size - 2
+            i = 0
+        
+        else:
+            if j == size:
+                j = 0
+            
+            if i < 0:
+                i = size - 1
+                
+        if magic_square[int(i)][int(j)]:
+            j = j - 2
+            i = i + 1
+            continue
+            
+        else:
+            magic_square[int(i)][int(j)] = num
+            num = num + 1
+                
+        j = j + 1
+        i = i - 1
+        
+    return magic_square 
+         
+               
+            
+print(getOddMagicSquare(3))
