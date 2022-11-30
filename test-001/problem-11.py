0f7f524537e1d5090cea489430f9281933a72949
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-11-30 15:54:02 +0530

    feat: :sparkles: add test solutions

--- test-001/problem-11.py
+++ test-001/problem-11.py
@@ -4,23 +4,17 @@
 
 f = lambda n: ((2 * n) + 4)
 
-def integrate (lower_range, upper_range, n):
-     
-    # Grid spacing
-    grid_space = (upper_range - lower_range) / n
-     
-    # Computing sum of first and last terms
-    # in above formula
-    s = (f(lower_range) + f(upper_range))
+def integrate (lower_range, upper_range, grid_divisions):
+    grid_space = (upper_range - lower_range) / grid_divisions
+
+    sum = (f(lower_range) + f(upper_range))
  
-    # Adding middle terms in above formula
     i = 1
-    while i < n:
+    while i < grid_divisions:
         s += 2 * f(lower_range + i * grid_space)
         i += 1
          
-    # h/2 indicates (b-a)/2n.
-    # Multiplying h/2 with s.
-    return ((grid_space / 2) * s)
+    
+    return ((grid_space / 2) * sum)
 
 print(integrate(5, 10, 10000))
