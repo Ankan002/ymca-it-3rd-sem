e380e9dd3af0a8584d4693845c07c64f34885ee8
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-12-03 11:33:40 +0530

    feat: :sparkles: add solution for integration

---
+++ assignment-01/problem-11/integrate.py
@@ -0,0 +1,20 @@
+"""
+    ? problem Statement: Q11 of assignment 1.
+"""
+
+f = lambda n: ((2 * n) + 4)
+
+def integrate (lower_range, upper_range, grid_divisions):
+    grid_space = (upper_range - lower_range) / grid_divisions
+
+    sum = (f(lower_range) + f(upper_range))
+ 
+    i = 1
+    while i < grid_divisions:
+        s += 2 * f(lower_range + i * grid_space)
+        i += 1
+         
+    
+    return ((grid_space / 2) * sum)
+
+print(integrate(5, 10, 10000))
