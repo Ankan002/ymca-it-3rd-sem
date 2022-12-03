883f2ba877ad3c057cbabd94fc7bea299844d05d
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-12-03 10:50:13 +0530

    feat: :sparkles: add solution for print odd numbers in a range

---
+++ assignment-01/problem-04/print-odd-positive-int.py
@@ -0,0 +1,17 @@
+def print_odds_in_range(start: int, end: int) -> None:
+    if start % 2 == 0:
+        start += 1
+        
+    while start <= end:
+        print(start)
+        start += 2
+        
+        
+        
+num1 = int(input("Enter number 1:"))
+num2 = int(input("Enter number 2:"))
+
+if num1 < 0 or num2 < 0:
+    print("Please provide positive numbers only....")
+else:
+    print_odds_in_range(num1, num2)
