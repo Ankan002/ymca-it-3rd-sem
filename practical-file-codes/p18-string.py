48b002664373c4d48cb10c510657045dab217e0a
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-14 18:26:33 +0530

    feat: :sparkles: add file practical 21

---
+++ practical-file-codes/p18-string.py
@@ -0,0 +1,26 @@
+"""
+    Name: Ankan Bhattacharya
+    Roll Number: 22001003505
+    
+    Question: Dealing with strings
+"""
+a="  How are you  "
+b="Manan"
+c=a+b  # string concatenation
+print(c)
+k=len(a)
+print(k)
+# extracting a part of the string
+c=a[:7]
+print(c)
+c=a[5:7]
+print(c)
+d=a[-10:-6]
+print(d)
+e=a.upper()
+print(e)
+f=a.lower()
+print(f)
+print(a.title()) # Title case
+f=a.strip() # for removing the leading blanks
+print(f)
