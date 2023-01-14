962f634eb128d344ee2ec2b877da79fccf208e69
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-14 17:25:36 +0530

    feat: :sparkles: add file practical 13

---
+++ practical-file-codes/p13-function.py
@@ -0,0 +1,18 @@
+"""
+    Name: Ankan Bhattacharya
+    Roll Number: 22001003505
+    
+    Question: Defining a function in python
+"""
+def f(x):
+    return x**3+3*x**2+8
+t=f(5)
+print(t)
+
+def bigger(x,y):
+    if x > y:
+        return x
+    else: 
+        return y
+k=bigger(10,17)
+print(k)
