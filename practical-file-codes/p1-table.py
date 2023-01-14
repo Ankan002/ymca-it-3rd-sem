5ff8e17eb21a4a3be73591df578e44cd7edfd1bd
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-14 16:31:31 +0530

    feat: :sparkles: add file practical 1

---
+++ practical-file-codes/p1-table.py
@@ -0,0 +1,13 @@
+"""
+    Name: Ankan Bhattacharya
+    Roll Number: 22001003505
+    
+    Question: Print table of a number
+"""
+
+n=int(input('Enter the number: \n'))
+k=1
+while (k <=10):
+    t=n*k
+    print(n,'*',k,'=',t)
+    k=k+1
