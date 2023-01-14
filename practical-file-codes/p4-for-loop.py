3f6ae7662ba8db75b873209b841e25d490dc3fe0
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-14 16:44:46 +0530

    feat: :sparkles: add file practical 4

---
+++ practical-file-codes/p4-for-loop.py
@@ -0,0 +1,22 @@
+"""
+    Name: Ankan Bhattacharya
+    Roll Number: 22001003505
+    
+    Question: working of for loop
+"""
+q=8
+for i in range (q):
+    print(i,end='  ')
+print('')
+for i in range (1,q):
+    print(i)
+print('')
+for i in range (2,q,1):
+    print(i)
+print('')
+for i in range (2,q,2):
+    print(i)
+print('')
+for i in range (10,q,2):
+    print(i)
+print('')
