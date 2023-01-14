cce9a93923f498037149de052d4572b0629c6370
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-14 17:33:39 +0530

    feat: :sparkles: add file practical 15

---
+++ practical-file-codes/p15-mathematical-function.py
@@ -0,0 +1,17 @@
+"""
+    Name: Ankan Bhattacharya
+    Roll Number: 22001003505
+    
+    Question: Mathematical functions
+"""
+import math
+k=math.gcd(15,80) # greatest common divisor
+print(k)
+t=math.fabs(-7.4)  # absolute value of a float number
+print(t)
+m=math.trunc(9.64)
+print(m)
+t=math.sqrt(7.8)
+print(t)
+t=math.sin(30*3.14159/180)
+print(t)
