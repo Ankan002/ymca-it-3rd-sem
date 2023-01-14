b43064f2417b284416ca74ff12819a3269ef727f
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-01-14 19:29:14 +0530

    feat: :sparkles: add file practical 29

---
+++ practical-file-codes/p29-tower-of-hanoy.py
@@ -0,0 +1,19 @@
+"""
+    Name: Ankan Bhattacharya
+    Roll Number: 22001003505
+    
+    Question: Solving tower of hanoi using the recursion
+"""
+def toh(n, init, temp, fin): 
+    if n == 1:
+        print(init,' ->',fin) #terminal definition
+    else:
+        # move n-1 disks from A to B, with C as temp
+        toh(n-1, init, fin, temp)
+        #move disk from A to C, with B as temp
+        toh(1, init, temp, fin)
+        # move n-1 disks from B to C, with A as temp
+        toh(n-1, temp, init, fin)
+# Tower of hanoi
+k=int(input('number of disks: \n'))
+toh(k,'A','B','C')
